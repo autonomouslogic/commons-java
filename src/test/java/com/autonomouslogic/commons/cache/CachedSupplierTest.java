0e8247d42de4ed2562f848848933d2992ab0d0ad
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-09-30 21:30:18 +0900

    Implemented simple CachedSupplier.

---
+++ src/test/java/com/autonomouslogic/commons/cache/CachedSupplierTest.java
@@ -0,0 +1,62 @@
+package com.autonomouslogic.commons.cache;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.function.Supplier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CachedSupplierTest {
+	Supplier<String> supplier;
+	CachedSupplier<String> cachedSupplier;
+
+	@BeforeEach
+	public void setup() {
+		supplier = spy(new Supplier<String>() {
+			@Override
+			public String get() {
+				return "result";
+			}
+		});
+		cachedSupplier = new CachedSupplier<>(supplier);
+	}
+
+	@Test
+	public void shouldDelegate() {
+		assertEquals("result", cachedSupplier.get());
+	}
+
+	@Test
+	public void shouldCacheValues() {
+		assertSame(cachedSupplier.get(), cachedSupplier.get());
+	}
+
+	@Test
+	public void shouldOnlyCallSupplierOnce() {
+		assertSame("result", cachedSupplier.get());
+		verify(supplier).get();
+		assertSame("result", cachedSupplier.get());
+		verifyNoMoreInteractions(supplier);
+	}
+
+	@Test
+	public void shouldCacheNullValues() {
+		supplier = spy(new Supplier<String>() {
+			@Override
+			public String get() {
+				return null;
+			}
+		});
+		cachedSupplier = new CachedSupplier<>(supplier);
+
+		assertNull(cachedSupplier.get());
+		verify(supplier).get();
+		assertNull(cachedSupplier.get());
+		verifyNoMoreInteractions(supplier);
+	}
+}
