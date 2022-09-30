743730224f8cd43ee5e47b0534da93607ec55b27
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-09-30 16:13:18 +0900

    Basic concatCopy.

--- src/main/java/com/autonomouslogic/commons/ListUtil.java
+++ src/main/java/com/autonomouslogic/commons/ListUtil.java
@@ -2,10 +2,23 @@ package com.autonomouslogic.commons;
 
 import com.autonomouslogic.commons.collection.ConcatList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListUtil {
 	public static <E> ConcatList<E> concat(List<E>... lists) {
 		return new ConcatList<>(List.of(lists));
+	}
+
+	public static <E> List<E> concatCopy(List<E>... lists) {
+		var n = Arrays.stream(lists)
+			.mapToInt(List::size)
+			.sum();
+		List<E> concat = new ArrayList<>(n);
+		for (List<E> list : lists) {
+			concat.addAll(list);
+		}
+		return concat;
 	}
 }
