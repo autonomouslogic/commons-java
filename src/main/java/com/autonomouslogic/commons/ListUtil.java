f1ec08e2ce1f4318059ac157136769b93a59e1e8
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-10-01 13:50:10 +0900

    Documentation.

--- src/main/java/com/autonomouslogic/commons/ListUtil.java
+++ src/main/java/com/autonomouslogic/commons/ListUtil.java
@@ -6,10 +6,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ListUtil {
+	/**
+	 * Concatenates the supplied list without explicitly copying every single element.
+	 * Limited functionality is provided for interacting with the underlying lists.
+	 * @param lists the lists to concatenate
+	 * @return a concatenated list instance
+	 * @param <E> the list element type
+	 */
 	public static <E> ConcatList<E> concat(List<E>... lists) {
 		return new ConcatList<>(List.of(lists));
 	}
 
+	/**
+	 * Concatenates the supplied lists into an {@link ArrayList} by copying every element.
+	 * @param lists the lists to concatenate
+	 * @return a concatenated list copy
+	 * @param <E> the list element type
+	 */
 	public static <E> List<E> concatCopy(List<E>... lists) {
 		var n = Arrays.stream(lists).mapToInt(List::size).sum();
 		List<E> concat = new ArrayList<>(n);
