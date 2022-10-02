0de3659724e477c292edf7690a826032ab5accd7
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-10-02 22:07:02 +0900

    build: Fixed Javadoc generation.

--- src/main/java/com/autonomouslogic/commons/ResourceUtil.java
+++ src/main/java/com/autonomouslogic/commons/ResourceUtil.java
@@ -36,7 +36,7 @@ public class ResourceUtil {
 	 * Loads a resource with the assumption that the path will be relative to one constructed from the class package and
 	 * name.
 	 * For instance, is the class <code>com.autonomouslogic.SomeClass</code> is provided, the path loaded will be
-	 * relative to <code>/com/autonomouslogic/SomeClass/{path}</path></code>.
+	 * relative to <code>/com/autonomouslogic/SomeClass/{path}</code>.
 	 * This is useful for loading resources during unit tests where you store resources for each test in a directory
 	 * named after the test class.
 	 * @param clazz the class to use for resource loading and contextual path
